3b551825b4ffb8d468cb8cfdb2bad202fa662359
jiajun zhang <3249769516@qq.com>
2019-07-09 18:57:02 +0800

    更新配置文件

--- gateway/src/main/java/com/goshop/gateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/goshop/gateway/gateway/GatewayApplication.java
@@ -3,9 +3,11 @@ package com.goshop.gateway.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +16,7 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 
 //	@Bean
 //	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
