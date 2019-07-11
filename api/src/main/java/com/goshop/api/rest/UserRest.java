451cb015c1a5556d56b0f058809bade22c874988
kekukele <1044586852@qq.com>
2019-07-11 23:34:25 +0800

    集成kafka

--- api/src/main/java/com/goshop/api/rest/UserRest.java
+++ api/src/main/java/com/goshop/api/rest/UserRest.java
@@ -2,6 +2,7 @@ package com.goshop.api.rest;
 
 import com.goshop.api.rpc.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,18 @@ public class UserRest {
     @Autowired
     IUserService userService;
 
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
     @RequestMapping("/findUserInfo")
     public String findUserInfo() {
         return userService.findUserInfo();
     }
 
+    @RequestMapping("/consumer")
+    public String consumer() {
+        kafkaTemplate.send("my-replicated-topic", "hello");
+        return "ddd";
+    }
 
 }
