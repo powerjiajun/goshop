851d3bf233467085d0fa427fea1b9eea5e2d55b0
jiajun zhang <3249769516@qq.com>
2019-07-25 18:09:29 +0800

    更改项目结构

---
+++ business/module-user/src/main/java/com/goshop/user/message/KafkaHandler.java
@@ -0,0 +1,39 @@
+package com.goshop.user.message;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class KafkaHandler {
+
+    @KafkaListener(topics = { "my-replicated-topic" }, groupId = "test")
+    public void handle(String message) {
+        System.out.println("[ 处理器开始处理消息 ]" + System.currentTimeMillis());
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println(message);
+
+        System.out.println("[ 处理器处理消息完成 ]" + System.currentTimeMillis());
+    }
+
+    @KafkaListener(topics = { "test3" }, groupId = "test")
+    public void handle(ConsumerRecord<String, String> record) {
+        System.out.println("[ 处理器开始处理消息 ]" + System.currentTimeMillis());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(record);
+
+        System.out.println("[ 处理器处理消息完成 ]" + System.currentTimeMillis());
+    }
+
+}
---
+++ business/module-user/src/main/java/com/goshop/user/service/impl/UserService.java
@@ -0,0 +1,23 @@
+package com.goshop.user.service.impl;
+
+import com.goshop.entity.User;
+import com.goshop.user.cache.RedisClientTemplate;
+import com.goshop.user.mapper.IUserMapper;
+import com.goshop.user.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+
+//    @Autowired
+//    RedisClientTemplate redisClientTemplate;
+
+    @Autowired
+    private IUserMapper userMapper;
+
+    @Override
+    public User findUserById() {
+        return userMapper.selectUser(1);
+    }
+}
