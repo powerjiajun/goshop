09a9bd3206bcc779503b4979803b23f4625efccf
jiajun zhang <3249769516@qq.com>
2019-07-26 15:56:29 +0800

    增加授权服务

--- business/api/src/main/java/com/goshop/api/rest/UserRest.java
+++ business/api/src/main/java/com/goshop/api/rest/UserRest.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/user")
 @RestController
 public class UserRest {
 
