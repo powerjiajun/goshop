94a24effcec75918b032595a1003b3ca7ff9c97b
jiajun zhang <3249769516@qq.com>
2019-07-29 14:17:32 +0800

    统一版本配置

---
+++ center/config/src/main/java/com/config/ConfigApplication.java
@@ -0,0 +1,14 @@
+package com.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+
+
+
+}
