d69ac41a0879f45c374372fdb90b8ee5b37ea603
jiajun zhang <3249769516@qq.com>
2019-08-01 15:22:13 +0800

    增加hystrix dashboard

--- business/api/src/main/java/com/goshop/api/ApiApplication.java
+++ business/api/src/main/java/com/goshop/api/ApiApplication.java
@@ -1,10 +1,15 @@
 package com.goshop.api;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@EnableCircuitBreaker
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -13,5 +18,15 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+	@Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }
